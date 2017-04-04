888f51248ae40009b256ff3ca548408aad23cbe9
Yuriy Bodnar <yuriy_bodnar@epam.com>
2017-04-04 00:46:46 -0400

    naive implementation of parser

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='comborun',
+    version='0.1',
+    packages=[''],
+    url='fridaynightcommits.com',
+    license='Apache License  Version 2.0',
+    author='ybodnar',
+    author_email='',
+    description=''
+)
